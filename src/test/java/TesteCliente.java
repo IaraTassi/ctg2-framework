60a8c4376a61578c4a9145eb38f2e9e7a916c772
Iara Tassi <iara.tassinh@gmail.com>
2023-03-23 18:11:46 -0300

    Alterações após aula de automação

--- src/test/java/TesteCliente.java
+++ src/test/java/TesteCliente.java
@@ -52,26 +52,77 @@ public class TesteCliente {
 
     //put
     @Test
-    @DisplayName("Autaliza um clienta já previamente cadastrado")
+    @DisplayName("Quando eu atualizar um cliente, então ele deve ser atualizado")
     public void atualizaCliente() {
 
-        String cadastroRealizado = "{\n" +
-                "  \"id\": 1005,\n" +
-                "  \"idade\": 15,\n" +
-                "  \"nome\": \"Pluto\",\n" +
+        String clienteParaCadastrar = "{\n" +
+                "  \"id\": 1004,\n" +
+                "  \"idade\": 25,\n" +
+                "  \"nome\": \"Minnie Mouse\",\n" +
                 "  \"risco\": 0\n" +
                 "}";
-        String respostaEsperada = "{\"1005\":{\"nome\":\"Pluto\",\"idade\":15,\"id\":1005,\"risco\":0}}";
+
+        String clienteAtualizado = "{\n" +
+                "  \"id\": 1004,\n" +
+                "  \"idade\": 33,\n" +
+                "  \"nome\": \"Minnie Mouse\",\n" +
+                "  \"risco\": 0\n" +
+                "}";
+
+        String respostaEsperada = "{\"1004\":{\"nome\":\"Minnie Mouse\",\"idade\":33,\"id\":1004,\"risco\":0}}";
+
+    // tinha esquecido de tudo isso por isso meu teste passou
+        given()
+                .contentType(ContentType.JSON)
+                .body(clienteParaCadastrar)
+        .when()
+                .post(enderecoApiCliente+endpointCliente)
+        .then()
+                .statusCode(201);
+        given()
+                .contentType(ContentType.JSON)
+                .body(clienteAtualizado)
+        .when()
+                .put(enderecoApiCliente+endpointCliente)
+        .then()
+                .statusCode(200)
+                .assertThat().body(containsString(respostaEsperada));
+
     }
 
     //delete
-    @Test@DisplayName("Deleta um cliente por id")
+    @Test
+    @DisplayName("Deleta um cliente por id")
     public void deletaCliente() {
-        String enderecoApiCliente = "http://localhost:8080/";
-        String endpointCliente = "cliente/1005";
 
+        String clienteParaCadastrar = "{\n" +
+                "  \"id\": 1004,\n" +
+                "  \"idade\": 33,\n" +
+                "  \"nome\": \"Minnie Mouse\",\n" +
+                "  \"risco\": 0\n" +
+                "}";
+        String respostaEsperada = "Cliente removido: { NOME: Minnie Mouse, IDADE: 33, ID: 1004 }";
 
+        given()
+                .contentType(ContentType.JSON)
+                .body(clienteParaCadastrar)
+        .when()
+                .post(enderecoApiCliente+endpointCliente)
+        .then()
+                .statusCode(201);
+
+        given()
+                .contentType(ContentType.JSON)
+        .when()
+                .delete(enderecoApiCliente+endpointCliente+"/1004")
+        .then()
+                .statusCode(200)
+                .body(new IsEqual<>(respostaEsperada));
     }
+
+
+
+
 
 
 }
